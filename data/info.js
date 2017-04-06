278e4a3205286317c78ac8831ce2d8ca60244f92
klervicn <corbel.klervi@gmail.com>
2017-04-06 17:54:28 +0200

    add data

---
+++ data/info.js
@@ -0,0 +1,9 @@
+const info = {
+  "address":"6 rue Delouvain, 75019 Paris",
+  "emailAddress":"corbel.klervi@gmail.com",
+  "phoneNumber":"06 78 85 35 10",
+  "driverLicense":"Permis B - Véhicule personnel"
+  "age":"24"
+}
+
+export default info
