48b541234ddb3531a78ce0fc3a9885559d9b4209
klervicn <corbel.klervi@gmail.com>
2017-04-06 14:04:13 +0200

    setup structure

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'next/link' // Gestion des liens
+
+export default class Home extends React.Component {
+  render() {
+    return (
+        <h1>Home</h1>
+    );
+  }
+}
