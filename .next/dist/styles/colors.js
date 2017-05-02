17f84987e9a59b406f7b4e124484dcfaf27a396f
klervicn <corbel.klervi@gmail.com>
2017-05-02 10:45:23 +0200

    Removed folder from repository

--- .next/dist/styles/colors.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var titlesColor = exports.titlesColor = "#bfbfbf";
-var labelColor = exports.labelColor = "#494949";
